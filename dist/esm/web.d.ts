317d3c3df4f10f770f6b2e13ad36e8d973fec0c9
Karson <11796725+karsf@users.noreply.github.com>
2024-03-20 01:15:16 +0800

    include dist folder

---
+++ dist/esm/web.d.ts
@@ -0,0 +1,16 @@
+import { WebPlugin } from "@capacitor/core";
+import type { InAppBrowserPlugin, OpenWebViewOptions, OpenOptions, GetCookieOptions, ClearCookieOptions } from "./definitions";
+export declare class InAppBrowserWeb extends WebPlugin implements InAppBrowserPlugin {
+    open(options: OpenOptions): Promise<any>;
+    clearCookies(options: ClearCookieOptions): Promise<any>;
+    getCookies(options: GetCookieOptions): Promise<any>;
+    openWebView(options: OpenWebViewOptions): Promise<any>;
+    executeScript({ code }: {
+        code: string;
+    }): Promise<any>;
+    close(): Promise<any>;
+    setUrl(options: {
+        url: string;
+    }): Promise<any>;
+    reload(): Promise<any>;
+}
